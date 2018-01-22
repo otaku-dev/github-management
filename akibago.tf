9a82c149e5b6c7917adbcae247e2319860479061
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2018-01-22 11:45:53 +0900

    Add akibago resouces

---
+++ akibago.tf
@@ -0,0 +1,21 @@
+resource "github_team" "akibago" {
+  name        = "akibago"
+  description = "Akiba.go team"
+  privacy     = "closed"
+}
+
+resource "github_repository" "akibago" {
+  name = "akibago"
+}
+
+resource "github_team_repository" "akibago_repo" {
+  team_id    = "${github_team.akibago.id}"
+  repository = "${github_repository.akibago.name}"
+  permission = "pull"
+}
+
+resource "github_team_membership" "member" {
+  team_id  = "${github_team.akibago.id}"
+  username = "ojiry"
+  role     = "maintainer"
+}
