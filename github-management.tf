06e75103c96dfb2bd3efec900ca363a7b71e2986
Ryoji Yoshioka <ryoji.yoshioka@quipper.com>
2018-01-24 16:43:15 +0900

    Add github-management.tf

---
+++ github-management.tf
@@ -0,0 +1,21 @@
+resource "github_repository" "github-management" {
+  name = "github-management"
+  has_issues = true
+  has_wiki = true
+  has_downloads = true
+}
+
+resource "github_branch_protection" "github-management" {
+  repository = "github-management"
+  branch = "master"
+  enforce_admins = true
+
+  required_status_checks {
+    strict = true
+    contexts = ["ci/circleci"]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews = true
+  }
+}
